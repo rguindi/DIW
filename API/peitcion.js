75da3e804dffea6c715f5aade6ff0d94f65669bd
rguindi <raul@hotmail.com>
2024-01-30 19:53:03 +0100

    api

---
+++ API/peitcion.js
@@ -0,0 +1,21 @@
+const url = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/303121?apikey=%0914wSpECtEQGFOurv9WiciZZgEqgqMhC3&language=es-es';
+
+// Realizar la solicitud utilizando fetch
+fetch(url)
+  .then(response => {
+    // Verificar si la solicitud fue exitosa (código 200)
+    if (!response.ok) {
+      throw new Error(`Error de red - ${response.status}`);
+    }
+    // Parsear la respuesta como JSON
+    return response.json();
+  })
+  .then(data => {
+    // Manejar los datos JSON aquí
+
+    console.log(data);
+  })
+  .catch(error => {
+    // Manejar errores de red o del servidor
+    console.error('Error:', error);
+  });
